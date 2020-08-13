1478fb48853d2aaf02b79504057cd6edc1068a68
PaulHeredia <spheredia1@espe.edu.ec>
2020-08-13 14:53:47 -0500

    Hoja de Vida

---
+++ paul.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=h1, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Paul Heredia</h1>
+    <p> Domicilio: Latacunga<br>
+        Edad: 20 años<br>
+        Hobbies: Animes, Musica y Futbol
+    </p>
+</body>
+</html>
